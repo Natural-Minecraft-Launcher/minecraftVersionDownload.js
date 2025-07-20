5b08fbbcd30946be8303702e85bae8e61cc1e3e6
sungbly1tsstt <114800438+sungbly1tsstt@users.noreply.github.com>
2025-07-20 05:32:51 -0400

    Add files via upload

---
+++ minecraftVersionDownload.js
@@ -0,0 +1,100 @@
+//copyright 2025 sungbly1tsstt
+function getVersionList(){
+    return($.ajax({
+        async: false,
+        cache: false,
+        type: "GET",
+        url: "https://launchermeta.mojang.com/mc/game/version_manifest.json"
+    }).responseJSON)
+}
+function getDownloadList(name,list,gameName){
+    console.log('list',list)
+    var index = -1;
+    for(var i = 0;i < list["versions"].length;i++){
+        if(list.versions[i].id == name){
+            index = i;
+        }
+    }
+    if(index == -1){
+        return("{}")
+    }
+    var downloadList = $.ajax({
+        async: false,
+        cache: false,
+        type: "GET",
+        url: list.versions[index].url
+    }).responseJSON
+    var downloadJSON = '['
+    var asset = $.ajax({
+        async: false,
+        cache: false,
+        type: "GET",
+        url: downloadList["assetIndex"].url
+    }).responseJSON
+    downloadJSON += '{"url":"'+list.versions[index].url+'","path":"versions/'+gameName+'/'+gameName+'.json"}'
+    console.log('asset',asset)
+    downloadJSON += ',{"url":"'+downloadList["assetIndex"].url+'","path":"assets/indexes/'+name+'.json"}'
+    for (var i = 0;i<Object.keys(asset.objects).length;i++){
+        downloadJSON += ',{"url":"https://resources.download.minecraft.net/'+asset["objects"][Object.keys(asset.objects)[i]].hash.substring(0,2)+"/"+asset["objects"][Object.keys(asset.objects)[i]].hash+'","path":"assets/objects/'+asset["objects"][Object.keys(asset.objects)[i]].hash.substring(0,2)+"/"+asset["objects"][Object.keys(asset.objects)[i]].hash+'"}'
+    }
+    console.log("client")
+    downloadJSON += ',{"url":"'+downloadList.downloads.client.url+'","path":"versions/'+gameName+'/'+gameName+'.jar"}'
+    console.log("lib")
+    for (var i = 0;i<downloadList.libraries.length;i++){
+        downloadJSON += ',{"url":"'+downloadList.libraries[i].downloads.artifact.url+'","path":"libraries/'+downloadList.libraries[i].downloads.artifact.path+'"}'
+    }
+    downloadJSON += "]"
+    return(downloadJSON)
+}
+function getVersionJavaVersion(name,list){
+    console.log('list',list)
+    var index = -1;
+    for(var i = 0;i < list["versions"].length;i++){
+        if(list.versions[i].id == name){
+            index = i;
+        }
+    }
+    if(index == -1){
+        return(0)
+    }
+    var downloadList = $.ajax({
+        async: false,
+        cache: false,
+        type: "GET",
+        url: list.versions[index].url
+    }).responseJSON
+    return(downloadList.javaVersion.majorVersion)
+}
+function getDownloadZipList(list){
+    var zipList = ""
+    for (var i = 0;i<list.libraries.length;i++){
+        if(typeof list.libraries[i].rules != 'undefined'){
+            if(list.libraries[i].rules[0].os.name == "windows"){
+                if(zipList.length != 0 ){
+                    zipList += ","
+                }
+                zipList += 'libraries/'+list.libraries[i].downloads.artifact.path
+            }
+        }
+    }
+    return(zipList)
+}
+function getVersionManifest(name,list){
+    console.log('list',list)
+    var index = -1;
+    for(var i = 0;i < list["versions"].length;i++){
+        if(list.versions[i].id == name){
+            index = i;
+        }
+    }
+    if(index == -1){
+        return("{}")
+    }
+    var downloadList = $.ajax({
+        async: false,
+        cache: false,
+        type: "GET",
+        url: list.versions[index].url
+    }).responseJSON
+    return(downloadList)
+}
